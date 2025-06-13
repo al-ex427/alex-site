1734ec4a50be0b2798f6d678be08e0f0146ef9c8
alex <147615443+al-ex427@users.noreply.github.com>
2025-06-13 20:14:51 +0300

    Commit 2

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+    <div style={{alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh'}}>
+      <h1>404</h1>
+      <p>We couldn't find the requested page</p>
+    </div>
+
+    </>
+  );
+}
