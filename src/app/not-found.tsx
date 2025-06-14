2f969da40c196cd139429c525a6ada3ed6df1908
alex <147615443+al-ex427@users.noreply.github.com>
2025-06-14 21:11:25 +0300

    New 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,22 @@
+import styles from "./page.module.scss";
+import background_img from '../../public/backgrounds/Tahoe-Dark.jpg'
 export default function NotFound() {
   return (
     <>
-    <div style={{alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh'}}>
-      <h1>404</h1>
-      <p>We couldn&apos;t find the requested page</p>
+    <div style={{
+      backgroundImage: `url(${background_img.src})`, 
+      width: '100%', 
+      height: '100vh', 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      }}>
+      <div className={`panel ${styles.panel}`} style={{height: '250px'}}>
+        <p style={{fontSize: '32px', fontWeight: '800'}}>404</p>
+        <p style={{fontSize: '18px', fontWeight: '500'}}>The requested page has not been found</p>
+      </div>
     </div>
+
 
     </>
   );
