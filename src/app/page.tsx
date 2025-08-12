ee40cc5b053d6e0e292a6a9ad55d74ccfde7ae31
alex <147615443+al-ex427@users.noreply.github.com>
2025-08-13 00:35:08 +0300

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ export default function Home() {
   return (
     <>
     <div className="page">
-      <h1 className="text-6xl font-extrabold py-12">Welcome to my site</h1>
+      <div className="w-full bg-cover bg-center bg-no-repeat bg-local  h-128 bg-[url(/assets/images/backgrounds/space_02.jpg)]">
+      <div className="flex justify-center content-center backdrop-blur-2xl h-full w-full">
+        
+      </div>
+          
+      </div>
+      
       <div className="h-full flex flex-col justify-center content-center">
         <iframe className="" src="https://discord.com/widget?id=1373259502555631669&theme=dark" width="350" height="500" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div className="z-5  w-[100%] bg-black/15 h-[7%] backdrop-blur-lg">
-        <div className="hidden sm:flex h-[100%] w-[100%] gap-16 text-[20px] font-400 top-0  flex-row justify-center items-center">
+        <div className="absolute z-5 w-[100%] border-b-1 border-b-white/25 bg-black/15 h-[7%] backdrop-blur-lg">
+        <div className="flex h-[100%] w-[100%] gap-16 text-[20px] font-400 flex-row justify-center items-center">
             <Link href="/">Home</Link>
             <a href="/dashboard">Dashboard</a>
             <Link href="/portofolio">Portofolio</Link>
