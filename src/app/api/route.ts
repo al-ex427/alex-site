6fe0491348f33af48b090730236389000c402991
alex <147615443+al-ex427@users.noreply.github.com>
2025-08-19 14:02:54 +0300

    move file to gitmanager

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,8 @@
+import { GetGitInfo } from "@/GitManager"
 import { NextRequest } from "next/server"
-import { execSync } from "child_process"
 
 
-export function GetGitInfo() {
-    try {
-        const curCommit = execSync('git rev-parse HEAD').toString().trim().slice(0,7)
-        const latestCommit = execSync('git ls-remote https://github.com/al-ex427/alex-site.git HEAD').toString().trim().slice(0,7)
-        return { "commit": curCommit, "latest": latestCommit }
-    } catch (e) {
-        console.log(`Error occured whilst getting git info:\n${e}`)
-        return { "commit": "N/A", "latest": "N/A", "message": "FAILED TO OBTAIN GIT INFO"}  
-    }
-}
+
 
 
 export async function GET(request:NextRequest) {
