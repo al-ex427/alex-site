1a136a80102a8245f3de4ea62b012eb7997aa9b2
alex <147615443+al-ex427@users.noreply.github.com>
2025-08-19 14:03:41 +0300

    lint complains about this

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server"
 
 
 
-export async function GET(request:NextRequest) {
+export async function GET() {
     const info = GetGitInfo()
     return new Response(JSON.stringify(info))
 }
